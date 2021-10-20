d072b641e7e7db4342a2285d923039b213f34f09
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-10-20 13:51:50 +0300

    Dictinary: move setting shorthands to crate level

--- src/dictionary/mod.rs
+++ src/dictionary/mod.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use crate::settings::{HarlawSettings, get_default_settings, get_no_markup_settings};
+use crate::settings::HarlawSettings;
 use crate::formatter;
 use crate::reader;
 
@@ -12,44 +12,32 @@ pub struct DictionaryEntry {
     pub definitions: Vec<String>
 }
 
-
-fn to_dictionary(filepath: &str, settings: HarlawSettings) -> Result<Vec<DictionaryEntry>, &'static str> {
+pub fn get_dictionary(filepath: &str, settings: HarlawSettings) -> Result<Vec<DictionaryEntry>, &'static str> {
     let lines = reader::read_dsl_file(filepath)?;
 
     Ok(formatter::format_entries(lines, settings))
-}
-
-pub fn get_dictionary(filepath: &str) -> Result<Vec<DictionaryEntry>, &'static str> {
-    let settings = get_default_settings();
-
-    to_dictionary(filepath, settings)
-}
-
-pub fn get_dictionary_without_markup(filepath: &str) -> Result<Vec<DictionaryEntry>, &'static str> {
-    let settings = get_no_markup_settings();
-
-    to_dictionary(filepath, settings)
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::settings::get_default_settings;
 
     #[test]
     fn errors_if_not_readable_dictionary() {
         let filename = "./imaginary-dictionary.dsl";
         
-        let result = get_dictionary(filename);
+        let result = get_dictionary(filename, get_default_settings());
 
         assert!(result.is_err());
     }
 
     #[test]
-    fn gets_default_dictionary_from_dsl() {
+    fn gets_dictionary_from_dsl() {
         let filename = "./src/dictionary/fixtures/zoega-excerpt.dsl";
         
-        let result = get_dictionary(filename).unwrap();
+        let result = get_dictionary(filename, get_default_settings()).unwrap();
     
     
         assert_eq!(result[0].word, "a");
@@ -58,20 +46,5 @@ mod tests {
 
         assert_eq!(result[1].word, "abbadis");
         assert_eq!(result[1].definitions[0], "(pl. -ar), f. <i>abbess</i>.");
-    }
-
-    #[test]
-    fn gets_no_markup_dictionary_from_dsl() {
-        let filename = "./src/dictionary/fixtures/zoega-excerpt.dsl";
-        
-        let result = get_dictionary_without_markup(filename).unwrap();
-    
-    
-        assert_eq!(result[0].word, "a");
-        assert_eq!(result[0].definitions[0], "a negative suffix to verbs, not;");
-        assert_eq!(result[0].definitions[1], "era útmakligt, at it is not unmeet that.");
-
-        assert_eq!(result[1].word, "abbadis");
-        assert_eq!(result[1].definitions[0], "(pl. -ar), f. abbess.");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,16 @@ mod reader;
 mod formatter;
 
 pub use dictionary::DictionaryEntry;
+pub use settings::{get_default_settings, get_no_markup_settings};
+
+pub fn get_dictionary(filepath: &str) -> Result<Vec<DictionaryEntry>, &'static str> {
+    let settings = get_default_settings();
+
+    dictionary::get_dictionary(filepath, settings)
+}
+
+pub fn get_dictionary_without_markup(filepath: &str) -> Result<Vec<DictionaryEntry>, &'static str> {
+    let settings = get_no_markup_settings();
+
+    dictionary::get_dictionary(filepath, settings)
+}
